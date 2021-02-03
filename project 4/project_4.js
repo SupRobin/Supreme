5170988c64eb1e261abd4a318fbdfa3bc129bb25
SupRobin <77211040+SupRobin@users.noreply.github.com>
2021-02-03 17:19:36 -0500

    Add files via upload

---
+++ project 4/project_4.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  animateDiv();
+
+});
+
+function makeNewPosition() {
+
+  // Get viewport dimensions (remove the dimension of the div)
+  var h = $(window).height() - 1200;
+  var w = $(window).width() - 1250 ;
+
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
+
+  return [nh, nw];
+
+}
+
+function animateDiv() {
+  var newq = makeNewPosition();
+  $('#circle').animate({
+    top: newq[0],
+    left: newq[1]
+  }, function() {
+    animateDiv();
+  });
+
+};
